342c59b46a6bb199d0f9075f94a5015eaad40a8a
Michael Baldeh <alagiebaldeh@yahoo.co.uk>
2020-05-16 12:30:22 +0200

    fomatted file of dogage

--- javascript/javascript1/week1/agecalc.js
+++ javascript/javascript1/week1/agecalc.js
@@ -1,7 +1,4 @@
-const yearOfBirth =  1990;
-const yearFuture =  2027;
+const yearOfBirth = 1990;
+const yearFuture = 2027;
 const age = yearFuture - yearOfBirth;
-console.log ('you will turn to be'  + age + 'year old by' + yearFuture);
- 
-
-
+console.log("you will turn to be" + age + "year old by" + yearFuture);

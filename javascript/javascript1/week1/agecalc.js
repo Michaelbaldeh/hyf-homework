edd087074571a3c78e42439831f4e8fbbc998690
Michael Baldeh <alagiebaldeh@yahoo.co.uk>
2020-05-16 03:39:36 +0200

    changed variable to const

--- javascript/javascript1/week1/agecalc.js
+++ javascript/javascript1/week1/agecalc.js
@@ -1,6 +1,6 @@
-let yearOfBirth =  1990;
-let yearFuture =  2027;
-let age = yearFuture - yearOfBirth;
+const yearOfBirth =  1990;
+const yearFuture =  2027;
+const age = yearFuture - yearOfBirth;
 console.log ('you will turn to be'  + age + 'year old by' + yearFuture);
  
 
